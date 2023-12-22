700cad70287bbb2e7b81a2557939e28f3b214cdb
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2023-12-22 21:18:54 +0900

    implement hacky version

--- go.mod
+++ go.mod
@@ -2,4 +2,11 @@ module github.com/antonve/opensalary-notifications-discord-bot
 
 go 1.19
 
-require github.com/kelseyhightower/envconfig v1.4.0 // indirect
+require (
+	github.com/cockroachdb/apd v1.1.0 // indirect
+	github.com/gtuk/discordwebhook v1.2.0 // indirect
+	github.com/kelseyhightower/envconfig v1.4.0 // indirect
+	github.com/leekchan/accounting v1.0.0 // indirect
+	github.com/pkg/errors v0.8.1 // indirect
+	github.com/shopspring/decimal v0.0.0-20180709203117-cd690d0c9e24 // indirect
+)
--- main.go
+++ main.go
@@ -4,9 +4,18 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"log"
 	"net/http"
 	"time"
+
+	"github.com/gtuk/discordwebhook"
+	"github.com/leekchan/accounting"
 )
+
+type Config struct {
+	ErrorWebhook  string
+	NotifyWebhook string
+}
 
 var seen map[int]Item = map[int]Item{}
 
@@ -21,6 +30,7 @@ type Item struct {
 	ID                 int     `json:"id"`
 	RoleTitle          string  `json:"role_title"`
 	RoleFocus          string  `json:"role_focus"`
+	Grade              string  `json:"grade"`
 	AnnualCompensation int     `json:"annual_compensation"`
 	AnnualSalary       int     `json:"annual_salary"`
 	AnnualBonus        int     `json:"annual_bonus"`
@@ -31,8 +41,17 @@ type Item struct {
 	Company            Company `json:"company"`
 }
 
+func (i *Item) URL() string {
+	return fmt.Sprintf("https://opensalary.jp/en/single-salary/%d", i.ID)
+}
+
 type Company struct {
 	Name string `json:"name_en"`
+	Slug string `json:"slug"`
+}
+
+func (c *Company) URL() string {
+	return fmt.Sprintf("https://opensalary.jp/en/companies/%s", c.Slug)
 }
 
 func fetchSalaryEntries() (*SalaryEntries, error) {
@@ -65,17 +84,36 @@ func updateSalaryEntries(newSalaries chan Item) {
 		if _, ok := seen[item.ID]; !ok {
 			seen[item.ID] = item
 
-			// Temporarily add logic to limit to 5 entries to avoid spamming during dev
-			if len(seen) == 5 {
+			// Temporarily add logic to limit to 1 entries to avoid spamming during dev
+			if len(seen) == 2 {
 				close(newSalaries)
-			} else if len(seen) <= 4 {
+			} else if len(seen) <= 1 {
 				newSalaries <- item
 			}
 		}
 	}
 }
 
+func sendNotification(url, username, content string, embeds []discordwebhook.Embed) {
+	message := discordwebhook.Message{
+		Username: &username,
+		Content:  &content,
+		Embeds:   &embeds,
+	}
+
+	err := discordwebhook.SendMessage(url, message)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
 func main() {
+	var config = &Config{
+		NotifyWebhook: "",
+		ErrorWebhook:  "",
+	}
+
+	ac := accounting.Accounting{Symbol: "¥", Precision: 0}
 	newSalaries := make(chan Item)
 	go func() {
 		for {
@@ -90,6 +128,46 @@ func main() {
 			return
 		}
 
-		fmt.Printf("%+v\n", item)
+		content := "New salary submitted"
+		companyURL := item.Company.URL()
+		itemURL := item.URL()
+		title := fmt.Sprintf("%s @ %s", ac.FormatMoney(item.AnnualCompensation), item.Company.Name)
+		labelRole := "Role"
+		labelFocus := "Focus"
+		labelGrade := "Grade"
+		labelSalary := "Salary"
+		labelBonus := "Bonus"
+		labelStock := "Stock"
+		labelSigningBonus := "Signing bonus"
+		labelYOE := "Years of experience"
+		labelYAC := "Years at company"
+		salary := ac.FormatMoney(item.AnnualSalary)
+		bonus := ac.FormatMoney(item.AnnualBonus)
+		stock := ac.FormatMoney(item.AnnualStock)
+		signingBonus := ac.FormatMoney(item.SigningBonusTotal)
+		yoe := fmt.Sprintf("%d", item.YearsOfExperience)
+		yac := fmt.Sprintf("%d", item.YearsAtCompany)
+		t := true
+		color := "15258703"
+		embeds := []discordwebhook.Embed{
+			{
+				Author: &discordwebhook.Author{Name: &item.Company.Name, Url: &companyURL},
+				Title:  &title,
+				Url:    &itemURL,
+				Color:  &color,
+				Fields: &[]discordwebhook.Field{
+					{Name: &labelRole, Value: &item.RoleTitle, Inline: &t},
+					{Name: &labelFocus, Value: &item.RoleFocus, Inline: &t},
+					{Name: &labelGrade, Value: &item.Grade, Inline: &t},
+					{Name: &labelSalary, Value: &salary, Inline: &t},
+					{Name: &labelBonus, Value: &bonus, Inline: &t},
+					{Name: &labelYOE, Value: &yoe, Inline: &t},
+					{Name: &labelStock, Value: &stock, Inline: &t},
+					{Name: &labelSigningBonus, Value: &signingBonus, Inline: &t},
+					{Name: &labelYAC, Value: &yac, Inline: &t},
+				},
+			},
+		}
+		sendNotification(config.NotifyWebhook, "OpenSalary", content, embeds)
 	}
 }
